47be29d4e3d1827bfbe1ef80049381c182eb9f52
alok9696 <134131062+alok9696@users.noreply.github.com>
2025-08-17 11:45:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,33 +59,34 @@ if page == "📡 Live Sensor Dashboard":
     st.subheader("📋 Latest Sensor Reading")
     st.write(df.iloc[-1])
 
-   col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
 
-with col1:
-    fig1 = px.line(df, y="Air temperature [K]", title="Air Temperature Over Time", markers=True)
-    st.plotly_chart(fig1, use_container_width=True)
+    with col1:
+        fig1 = px.line(df, y="Air temperature [K]", title="Air Temperature Over Time", markers=True)
+        st.plotly_chart(fig1, use_container_width=True)
 
-with col2:
-    fig2 = px.line(df, y="Process temperature [K]", title="Process Temperature Over Time", markers=True)
-    st.plotly_chart(fig2, use_container_width=True)
+    with col2:
+        fig2 = px.line(df, y="Process temperature [K]", title="Process Temperature Over Time", markers=True)
+        st.plotly_chart(fig2, use_container_width=True)
 
-st.subheader("🔍 Sensor Feature Relationships")
+    st.subheader("🔍 Sensor Feature Relationships")
 
-if st.button("Generate Scatter Matrix"):
-    fig_matrix = px.scatter_matrix(
-        df,
-        dimensions=[
-            "Air temperature [K]",
-            "Process temperature [K]",
-            "Rotational speed [rpm]",
-            "Torque [Nm]",
-            "Tool wear [min]"
-        ],
-        title="Sensor Feature Relationships",
-        color="Type",
-        height=600
-    )
-    st.plotly_chart(fig_matrix, use_container_width=True)
+    if st.button("Generate Scatter Matrix"):
+        fig_matrix = px.scatter_matrix(
+            df,
+            dimensions=[
+                "Air temperature [K]",
+                "Process temperature [K]",
+                "Rotational speed [rpm]",
+                "Torque [Nm]",
+                "Tool wear [min]"
+            ],
+            title="Sensor Feature Relationships",
+            color="Type",
+            height=600
+        )
+        st.plotly_chart(fig_matrix, use_container_width=True)
+
 # --- Page 2: Machine Failure Prediction ---
 elif page == "🛠️ Failure Prediction":
     st.header("🛠️ Machine Failure Prediction Dashboard")
@@ -203,8 +204,3 @@ elif page == "🛠️ Failure Prediction":
                     ax.tick_params(labelbottom=True, labelleft=True)
         pairplot.figure.tight_layout()
         st.pyplot(pairplot.figure)
-
-
-
-
-
